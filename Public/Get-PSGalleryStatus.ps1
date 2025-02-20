ce9a88383c84239a8709fc373fe796566636ff82
alain <alainQtec@gmail.com>
2025-02-20 23:59:46 +0000

    Create Get-PSGalleryStatus.ps1

---
+++ Public/Get-PSGalleryStatus.ps1
@@ -0,0 +1,15 @@
+﻿function Get-PSGalleryStatus {
+  [CmdletBinding()]
+  param (
+  )
+
+  begin {
+  }
+
+  process {
+    # TODO: parse it from: https://github.com/PowerShell/PowerShellGallery/blob/master/psgallery_status.md
+  }
+
+  end {
+  }
+}
