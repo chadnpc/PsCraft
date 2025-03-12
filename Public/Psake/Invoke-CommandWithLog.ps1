910b2d6a2b5228b82b5e7f6b764e33a076322771
alain <chadnpc@outlook.com>
2025-03-12 15:28:57 +0200

    ~moved to base

--- Public/Psake/Invoke-CommandWithLog.ps1
+++
@@ -1,9 +0,0 @@
-﻿function Invoke-CommandWithLog {
-  [CmdletBinding()]
-  Param (
-    [parameter(Mandatory, Position = 0)]
-    [ScriptBlock]$ScriptBlock
-  )
-  Write-BuildLog -Command ($ScriptBlock.ToString() -join "`n");
-  $ScriptBlock.Invoke()
-}
