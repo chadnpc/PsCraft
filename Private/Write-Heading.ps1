4170190434029baa3bf24fc9a1ca86a442df2816
alain <chadnpc@outlook.com>
2025-03-12 15:28:22 +0200

    Create Write-Heading.ps1

---
+++ Private/Write-Heading.ps1
@@ -0,0 +1,21 @@
+﻿function Write-Heading {
+  param(
+    [parameter(Position = 0, Mandatory = $true)]
+    [ValidateNotNullOrWhiteSpace()]
+    [String]$Title,
+
+    [parameter(Position = 1)]
+    [Switch]$Passthru
+  )
+  process {
+    $msgList = @(
+      ''
+      "##[section] $(Get-Elapsed) $Title"
+    ) -join "`n"
+    if ($Passthru) {
+      $msgList
+    } else {
+      $msgList | Write-Host -f Green
+    }
+  }
+}
